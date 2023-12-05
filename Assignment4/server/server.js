1112c6b27b4fcd0ab951737e6d1a9c8fa7d7f2d4
amahdi4 <amahdi4@uwo.ca>
2023-12-05 16:48:47 -0500

    feat: Medals
    Added the routes for creating a new medal, getting all medals, granting the user a medal within a lobby, route to get which medals a user has in a specific lobby.

--- Assignment4/server/server.js
+++ Assignment4/server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./api/users');
 const lobbiesRouter = require('./api/lobby');
 const charactersRouter = require('./api/character');
+const medalsRouter = require('./api/medal');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/users', usersRouter);
 app.use('/api/lobby', lobbiesRouter);
 app.use('/api/character', charactersRouter);
+app.use('/api/medal', medalsRouter);
 
 app.listen(3000, () => console.log('Server is listening on port 3000...'));
 
