9fbb32e067aec99b914c5ae5eb69a9d7410fe745
amahdi4 <amahdi4@uwo.ca>
2023-12-06 16:52:44 -0500

    resolving merge
    Resolving merge conflict.

--- Assignment4/client/src/App.js
+++ Assignment4/client/src/App.js
@@ -2,20 +2,11 @@ import MainPg from './pages/MainPg';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
-<<<<<<< HEAD
 import Admin from './pages/Admin';
 import AdminSignup from './pages/AdminSignup';
 import AdminLogin from './pages/AdminLogin';
-=======
-
 import CharacterList from './pages/collectiveverse';
-
 import MedalShop from './pages/MedalShop';
-
-
-
-
->>>>>>> Anees
 import{BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
@@ -26,14 +17,11 @@ function App() {
          <Route path="/Login" element={<Login />} />
          <Route path="/SignUp" element={<SignUp />} />
          <Route path="/Home" element={<Home />} />
-<<<<<<< HEAD
          <Route path="/Admin" element={<Admin />} />
          <Route path="/AdminSignup" element={<AdminSignup />} />
          <Route path="/AdminLogin" element={<AdminLogin />} />
-=======
          <Route path="/collectiveverse" element={<CharacterList />} />
          <Route path="/MedalShop" element={<MedalShop />} />
->>>>>>> Anees
        </Routes>
    </Router>
    
