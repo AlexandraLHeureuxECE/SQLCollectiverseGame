eec53903c2e145b5987d06c009ea2b02ab023236
amahdi4 <amahdi4@uwo.ca>
2023-12-06 13:05:23 -0500

    Admin page
    Setup admin page

--- Assignment4/client/src/App.js
+++ Assignment4/client/src/App.js
@@ -2,6 +2,7 @@ import MainPg from './pages/MainPg';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import Admin from './pages/Admin';
 import{BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
          <Route path="/Login" element={<Login />} />
          <Route path="/SignUp" element={<SignUp />} />
          <Route path="/Home" element={<Home />} />
+         <Route path="/Admin" element={<Admin />} />
        </Routes>
    </Router>
    
---
+++ Assignment4/client/src/pages/Admin.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+
+
+
+function Admin() {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+    </div>
+  );
+}
+
+export default Admin;
